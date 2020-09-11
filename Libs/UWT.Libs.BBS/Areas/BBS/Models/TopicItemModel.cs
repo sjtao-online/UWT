7b620c2cbc9c69cf6e67c56b35c2f7932cfc1b90
sjtao <shen.juntao@hotmail.com>
2020-09-11 08:27:17 +0800

    445

---
+++ Libs/UWT.Libs.BBS/Areas/BBS/Models/TopicItemModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UWT.Templates.Models.Basics;
+
+namespace UWT.Libs.BBS.Areas.BBS.Models
+{
+    public class TopicItemModel : IdModel
+    {
+        public int UserId { get; set; }
+        public string Content { get; set; }
+        public DateTime PostTime { get; set; }
+        public int FlowIndex { get; set; }
+    }
+}
