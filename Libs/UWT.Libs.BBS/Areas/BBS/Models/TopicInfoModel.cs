c3628d44a4419e05e63b806c5c522aa33c66223a
hehe <hehe@sub.com>
2020-09-13 19:49:11 +0800

    论坛相关

---
+++ Libs/UWT.Libs.BBS/Areas/BBS/Models/TopicInfoModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UWT.Templates.Models.Basics;
+
+namespace UWT.Libs.BBS.Areas.BBS.Models
+{
+    public class TopicInfoModel : TitleIdModel
+    {
+        public bool IsHot { get; set; }
+        public int TopLevel { get; set; }
+        public int VisitorCount { get; set; }
+        public int CommitCount { get; set; }
+    }
+}
--- Libs/UWT.Libs.BBS/Areas/BBS/Models/TopicListItemModel.cs
+++ Libs/UWT.Libs.BBS/Areas/BBS/Models/TopicListItemModel.cs
@@ -5,14 +5,10 @@ using UWT.Templates.Models.Basics;
 
 namespace UWT.Libs.BBS.Areas.BBS.Models
 {
-    public class TopicListItemModel : TitleIdModel
+    public class TopicListItemModel : TopicInfoModel
     {
         public IconNameIdModel UserInfo { get; set; }
         public LastCommitInfo LastCommitInfo { get; set; }
-        public bool IsHot { get; set; }
-        public int TopLevel { get; set; }
-        public int VisitorCount { get; set; }
-        public int CommitCount { get; set; }
         public DateTime CreateTime { get; set; }
     }
 }
